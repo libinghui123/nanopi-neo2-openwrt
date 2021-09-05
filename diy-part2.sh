6fc607fe82a55b1563470a189c97fe0845a60387
huangzulin <huangzulin@users.noreply.github.com>
2021-09-05 10:57:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,11 +13,6 @@
 # Modify default IP
 # sed -i 's/192.168.1.1/192.168.1.111/g' package/base-files/files/bin/config_generate
 
-# 修复核心及添加温度显示
-sed -i 's|pcdata(boardinfo.system or "?")|luci.sys.exec("uname -m") or "?"|g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
-sed -i 's/or "1"%>/or "1"%> ( <%=luci.sys.exec("expr `cat \/sys\/class\/thermal\/thermal_zone0\/temp` \/ 1000") or "?"%> \&#8451; ) /g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
-
-
 # Clone community packages to package/community
 mkdir package/community
 pushd package/community
